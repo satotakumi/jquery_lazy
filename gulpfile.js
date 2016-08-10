bfaba25ba8ae80ed97791138560b259718ef158d
Sota Yamashita <sota.yamashita@gmail.com>
2016-08-10 09:16:52 +0900

    Minify jquery.lazy.js with gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var pkg = require('./package.json');
+
+gulp.task('build', function() {
+    var banner = ['/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.homepage %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    '',
+    ''].join('\n');
+
+    gulp.src('jquery.lazy.js')
+        .pipe($.header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist'))
+        .pipe($.uglify())
+        .pipe($.header(banner, { pkg: pkg }))
+        .pipe($.rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['build']);
